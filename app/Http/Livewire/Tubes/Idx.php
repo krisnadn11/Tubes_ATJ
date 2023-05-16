3b45b48c541c6ce11b57bf455d1e8ebf426b279d
krisnadn11 <krisnadn132@upi.edu>
2023-05-16 08:26:03 +0700

    Menyelesaikan landing page

---
+++ app/Http/Livewire/Tubes/Idx.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Tubes;
+
+use Livewire\Component;
+
+class Idx extends Component
+{
+    public function render()
+    {
+        return view('livewire.tubes.idx')->layout('adminlte::page');
+    }
+}
---
+++ resources/views/livewire/tubes/idx.blade.php
@@ -0,0 +1,18 @@
+<div>
+    @section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <b>PROJECT</b> | Kelompok 7
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
+</div>
